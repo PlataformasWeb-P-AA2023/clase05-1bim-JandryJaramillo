f3b1dd5910f5e2cce846ec1531a1ddda1c9d3af2
JandryJaramillo <jandry_josue16@hotmail.com>
2023-05-08 18:39:34 -0500

    salu3

--- ejemplo-flask/app.py
+++ ejemplo-flask/app.py
@@ -8,6 +8,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/persona/<id>')
+@app.route('/persona/<id>/spocks/lan/master')
 def persona(id):
     return render_template('persona.html', id=id)
